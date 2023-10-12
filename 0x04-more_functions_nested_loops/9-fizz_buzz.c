a81f3393d3b3dbbf534d4f37ccfb3324069bceed
KatsCapital <Katlego.Manala@Live.com>
2023-10-12 13:42:47 -0700

    IsUppper

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 1 2 Fizz 4 Buzz Fizz 7 8 
+ * Fizz Buzz 11 Fizz
+ * 13 14 FizzBuzz 16 17 Fizz 19 Buzz
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
